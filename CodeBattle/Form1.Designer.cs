2402e3a9f38bb496281eb6da49e36b120478d363
Adharv Arun <86561401+adharv2645@users.noreply.github.com>
2022-02-26 13:13:51 +0400

    Fix errors

--- CodeBattle/Form1.Designer.cs
+++ CodeBattle/Form1.Designer.cs
@@ -34,14 +34,41 @@
             this.radLabel1 = new Telerik.WinControls.UI.RadLabel();
             this.closeBtn = new System.Windows.Forms.Button();
             this.panel2 = new System.Windows.Forms.Panel();
+            this.radButton5 = new Telerik.WinControls.UI.RadButton();
+            this.radButton4 = new Telerik.WinControls.UI.RadButton();
+            this.radButton3 = new Telerik.WinControls.UI.RadButton();
+            this.radButton2 = new Telerik.WinControls.UI.RadButton();
             this.radButton1 = new Telerik.WinControls.UI.RadButton();
             this.label1 = new System.Windows.Forms.Label();
             this.crystalDarkTheme1 = new Telerik.WinControls.Themes.CrystalDarkTheme();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox7 = new System.Windows.Forms.PictureBox();
+            this.radButton6 = new Telerik.WinControls.UI.RadButton();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.radLabel2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.radLabel1)).BeginInit();
             this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.radButton5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radButton4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radButton3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radButton2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.radButton1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radButton6)).BeginInit();
             this.SuspendLayout();
             // 
             // panel1
@@ -62,9 +89,9 @@
             this.radLabel2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.radLabel2.Location = new System.Drawing.Point(34, 16);
             this.radLabel2.Name = "radLabel2";
-            this.radLabel2.Size = new System.Drawing.Size(48, 27);
+            this.radLabel2.Size = new System.Drawing.Size(187, 27);
             this.radLabel2.TabIndex = 3;
-            this.radLabel2.Text = "Title";
+            this.radLabel2.Text = "Monuments of UAE";
             this.radLabel2.ThemeName = "CrystalDark";
             this.radLabel2.Click += new System.EventHandler(this.radLabel2_Click);
             // 
@@ -98,6 +125,10 @@
             // panel2
             // 
             this.panel2.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.panel2.Controls.Add(this.radButton5);
+            this.panel2.Controls.Add(this.radButton4);
+            this.panel2.Controls.Add(this.radButton3);
+            this.panel2.Controls.Add(this.radButton2);
             this.panel2.Controls.Add(this.radButton1);
             this.panel2.Controls.Add(this.label1);
             this.panel2.Location = new System.Drawing.Point(12, 94);
@@ -105,14 +136,56 @@
             this.panel2.Size = new System.Drawing.Size(176, 280);
             this.panel2.TabIndex = 1;
             // 
+            // radButton5
+            // 
+            this.radButton5.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.radButton5.Location = new System.Drawing.Point(3, 212);
+            this.radButton5.Name = "radButton5";
+            this.radButton5.Size = new System.Drawing.Size(170, 52);
+            this.radButton5.TabIndex = 7;
+            this.radButton5.Text = "Jumeriah Archaeological Park ";
+            this.radButton5.ThemeName = "CrystalDark";
+            this.radButton5.Click += new System.EventHandler(this.radButton5_Click);
+            // 
+            // radButton4
+            // 
+            this.radButton4.Location = new System.Drawing.Point(3, 168);
+            this.radButton4.Name = "radButton4";
+            this.radButton4.Size = new System.Drawing.Size(171, 38);
+            this.radButton4.TabIndex = 6;
+            this.radButton4.Text = "Burj Khalifa";
+            this.radButton4.ThemeName = "CrystalDark";
+            this.radButton4.Click += new System.EventHandler(this.radButton4_Click);
+            // 
+            // radButton3
+            // 
+            this.radButton3.Location = new System.Drawing.Point(3, 124);
+            this.radButton3.Name = "radButton3";
+            this.radButton3.Size = new System.Drawing.Size(171, 38);
+            this.radButton3.TabIndex = 5;
+            this.radButton3.Text = "Al Fahidi Fort";
+            this.radButton3.ThemeName = "CrystalDark";
+            this.radButton3.Click += new System.EventHandler(this.radButton3_Click);
+            // 
+            // radButton2
+            // 
+            this.radButton2.Location = new System.Drawing.Point(3, 80);
+            this.radButton2.Name = "radButton2";
+            this.radButton2.Size = new System.Drawing.Size(171, 38);
+            this.radButton2.TabIndex = 4;
+            this.radButton2.Text = "Monuments of UAE";
+            this.radButton2.ThemeName = "CrystalDark";
+            this.radButton2.Click += new System.EventHandler(this.radButton2_Click);
+            // 
             // radButton1
             // 
             this.radButton1.Location = new System.Drawing.Point(3, 36);
             this.radButton1.Name = "radButton1";
             this.radButton1.Size = new System.Drawing.Size(171, 38);
             this.radButton1.TabIndex = 3;
-            this.radButton1.Text = "Button";
+            this.radButton1.Text = "What are Monuments?";
             this.radButton1.ThemeName = "CrystalDark";
+            this.radButton1.Click += new System.EventHandler(this.radButton1_Click);
             // 
             // label1
             // 
@@ -125,12 +198,146 @@
             this.label1.TabIndex = 0;
             this.label1.Text = "Navigation";
             // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox1.Location = new System.Drawing.Point(-4, 364);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(806, 86);
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox2.BackgroundImage")));
+            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox2.Location = new System.Drawing.Point(210, 111);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(565, 221);
+            this.pictureBox2.TabIndex = 5;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Visible = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(309, 111);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(388, 57);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Monuments of UAE";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(387, 168);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(195, 35);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "By Code of Duty";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(411, 203);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(147, 23);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "JSS Private School";
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox3.BackgroundImage")));
+            this.pictureBox3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox3.Location = new System.Drawing.Point(223, 111);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(565, 221);
+            this.pictureBox3.TabIndex = 9;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Visible = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox4.BackgroundImage")));
+            this.pictureBox4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox4.Location = new System.Drawing.Point(500, 168);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(254, 155);
+            this.pictureBox4.TabIndex = 10;
+            this.pictureBox4.TabStop = false;
+            this.pictureBox4.Visible = false;
+            this.pictureBox4.Click += new System.EventHandler(this.pictureBox4_Click_1);
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox6.BackgroundImage")));
+            this.pictureBox6.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox6.Location = new System.Drawing.Point(210, 56);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(578, 302);
+            this.pictureBox6.TabIndex = 11;
+            this.pictureBox6.TabStop = false;
+            this.pictureBox6.Visible = false;
+            this.pictureBox6.Click += new System.EventHandler(this.pictureBox6_Click);
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox5.BackgroundImage")));
+            this.pictureBox5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox5.Location = new System.Drawing.Point(210, 56);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(578, 302);
+            this.pictureBox5.TabIndex = 12;
+            this.pictureBox5.TabStop = false;
+            this.pictureBox5.Visible = false;
+            this.pictureBox5.Click += new System.EventHandler(this.pictureBox5_Click);
+            // 
+            // pictureBox7
+            // 
+            this.pictureBox7.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox7.BackgroundImage")));
+            this.pictureBox7.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox7.Location = new System.Drawing.Point(210, 56);
+            this.pictureBox7.Name = "pictureBox7";
+            this.pictureBox7.Size = new System.Drawing.Size(578, 302);
+            this.pictureBox7.TabIndex = 0;
+            this.pictureBox7.TabStop = false;
+            this.pictureBox7.Visible = false;
+            this.pictureBox7.Click += new System.EventHandler(this.pictureBox7_Click);
+            // 
+            // radButton6
+            // 
+            this.radButton6.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.radButton6.Location = new System.Drawing.Point(31, 57);
+            this.radButton6.Name = "radButton6";
+            this.radButton6.Size = new System.Drawing.Size(128, 31);
+            this.radButton6.TabIndex = 8;
+            this.radButton6.Text = "Home";
+            this.radButton6.ThemeName = "CrystalDark";
+            this.radButton6.Click += new System.EventHandler(this.radButton6_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.radButton6);
+            this.Controls.Add(this.pictureBox7);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox6);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
@@ -144,8 +351,21 @@
             ((System.ComponentModel.ISupportInitialize)(this.radLabel1)).EndInit();
             this.panel2.ResumeLayout(false);
             this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.radButton5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radButton4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radButton3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radButton2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.radButton1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radButton6)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -159,5 +379,20 @@
         private Telerik.WinControls.UI.RadLabel radLabel2;
         private Telerik.WinControls.UI.RadLabel radLabel1;
         private Telerik.WinControls.UI.RadButton radButton1;
+        private PictureBox pictureBox1;
+        private PictureBox pictureBox2;
+        private Telerik.WinControls.UI.RadButton radButton2;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private PictureBox pictureBox3;
+        private PictureBox pictureBox4;
+        private Telerik.WinControls.UI.RadButton radButton3;
+        private PictureBox pictureBox6;
+        private Telerik.WinControls.UI.RadButton radButton4;
+        private PictureBox pictureBox5;
+        private Telerik.WinControls.UI.RadButton radButton5;
+        private PictureBox pictureBox7;
+        private Telerik.WinControls.UI.RadButton radButton6;
     }
 }
--- CodeBattle/Form1.cs
+++ CodeBattle/Form1.cs
@@ -18,7 +18,7 @@ namespace CodeBattle
         {
             if (e.Button == MouseButtons.Left)
                 ReleaseCapture();
-                SendMessage(this.Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
+            SendMessage(this.Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
         }
 
         private void panel1_Paint(object sender, PaintEventArgs e)
@@ -38,10 +38,99 @@ namespace CodeBattle
 
         private void Form1_Load(object sender, EventArgs e)
         {
-
+            pictureBox4.Visible = false;
         }
 
         private void radLabel2_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void radLabel5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox4_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox7_Click(object sender, EventArgs e)
+        {
+
+        }
+        
+        private void radButton7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radButton6_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Visible=true;
+            pictureBox2.Visible = false;
+            pictureBox3.Visible = false;
+            pictureBox4.Visible = false;
+            pictureBox5.Visible = false;
+            pictureBox6.Visible = false;
+            pictureBox7.Visible = false;
+            label2.Visible = true;
+            label3.Visible = true;
+        }
+
+        private void radButton1_Click(object sender, EventArgs e)
+        {
+            pictureBox2.Visible = true;
+            pictureBox3.Visible = true;
+            label2.Visible = false;
+            label3.Visible = false;
+        }
+
+        private void radButton2_Click(object sender, EventArgs e)
+        {
+            pictureBox2.Visible = false;
+            pictureBox3.Visible = false;
+            pictureBox4.Visible = true;
+            pictureBox4.BringToFront();
+            pictureBox3.Visible = true;
+        }
+
+        private void radButton3_Click(object sender, EventArgs e)
+        {
+            pictureBox4.Visible = false;
+            pictureBox3.Visible = false;
+            pictureBox6.Visible = true;
+        }
+
+        private void radButton4_Click(object sender, EventArgs e)
+        {
+            pictureBox6.Visible=false;
+            pictureBox5.Visible = true;
+        }
+
+        private void radButton5_Click(object sender, EventArgs e)
+        {
+            pictureBox5.Visible=false;
+            pictureBox7.Visible = true;
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
         {
 
         }
